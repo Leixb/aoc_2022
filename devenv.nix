b76fcfc295abda44d41d3ce6996278214e23b2e6
LeixB <abone9999@gmail.com>
2023-08-28 13:48:38 +0200

    devenv: use languages

--- devenv.nix
+++ devenv.nix
@@ -1,15 +1,23 @@
 { pkgs, ... }:
 
 {
-  packages = with pkgs; [
-    gawk
-    go
-    gcc
-    python3
-    rustc
-    rust-analyzer
-    cargo
-  ];
+  # packages = with pkgs; [
+  #   gawk
+  #   go
+  #   gcc
+  #   python3
+  #   rustc
+  #   rust-analyzer
+  #   cargo
+  # ];
 
-  languages.haskell.enable = true;
+  languages = {
+    haskell.enable = true;
+    julia.enable = true;
+    go.enable = true;
+    rust.enable = true;
+    cplusplus.enable = true;
+    c.enable = true;
+    python.enable = true;
+  };
 }
